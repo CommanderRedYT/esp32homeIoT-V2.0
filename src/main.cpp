52b1f8c8b94440235446950048f54321d06215e6
CommanderRedYT <ccommanderred@gmail.com>
2021-01-07 16:01:52 +0100

    Moved settings to external file

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,5 @@
 #include <Arduino.h>
-
-//Start User Settings
-
-//#define _DEBUG_ //Uncomment if you want to enable debug mode
-
-    //WiFi
-    const char *ssid = "";
-    const char *password = "";
-    const char *WIFI_SSID = "";
-    const char *WIFI_PASS = "";
-
-    //thinger.io
-    #define USERNAME ""
-    #define DEVICE_ID ""
-    #define DEVICE_CREDENTIAL ""
-
-    //sinric.pro
-    #define APP_KEY ""    // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx" //Sinric.pro
-    #define APP_SECRET "" // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
-    #define LIGHT_ID ""   // Should look like "5dc1564130xxxxxxxxxxxxxx"
-
-    //WakeOnLan
-    const char *MACAddress = "";
-
-    //Custom HTTP GET-Request (I use it in combination with IFTTT to control a TP-Link Smartplug)
-    const char *url = "";
-
-    //DHT-config
-    #define DHTPIN 19
-    #define DHTTYPE DHT11
-
-    //External Relay
-    #define RELAY 4
-
-    //IR-pin (connected to transistor to control ir led)
-    const uint16_t SEND_PIN = 5;
-
-    //Serial Monitor Baud Rate
-    #define BAUD_RATE 115200
-
-
-//End User Settings
+#include "credentials.h"
 
 #ifdef _DEBUG_
 #define DEBUG_ESP_PORT Serial
@@ -453,6 +412,11 @@ void setup()
             }
         }
         Serial.println("ledstripSwitch");
+    };
+    thing["ifttt_hum"] >> [](pson &out) {
+        out["value1"] = dht.readHumidity();
+        out["value2"] = dht.readTemperature();
+        out["value3"] = "none";
     };
     delay(100);
     dht.begin();
